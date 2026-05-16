16e54b5e71fc8600356d80a9dc406938aa29017f
ApuKM <apukumar180@gmail.com>
2026-05-16 21:23:38 +0600

    Project setup & get courses data from db with get apis

---
+++ index.js
@@ -0,0 +1,59 @@
+const dotenv = require("dotenv")
+const express = require('express')
+
+dotenv.config();
+const app = express()
+const port = process.env.PORT
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("mentoradb")
+    const coursesCollection = db.collection("courses")
+
+    app.get("/courses", async(req, res) => {
+        const cursor = coursesCollection.find({})
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get("/courses/:courseId", async(req, res) => {
+        const {courseId} = req.params;
+        const result = await coursesCollection.findOne({_id: new ObjectId(courseId)})
+        res.send(result)
+    })
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
